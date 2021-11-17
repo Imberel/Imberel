cdf35344dc110630ab1401d18194f83906010181
Binkap S <real.desert.tiger@gmail.com>
2021-11-17 16:20:11 +0100

    improved migrations

---
+++ database/migrations/CXOiT_2021_11_17_CXOiT8lZ2OI3Wb9.php
@@ -0,0 +1,53 @@
+<?php
+
+use Imberel\Imberel\Core\Database\Schema\Blueprint;
+use Imberel\Imberel\Core\Database\Schema\Schema;
+
+/**
+ *  CXOiT_2021_11_17_CXOiT8lZ2OI3Wb9 Class
+ *
+ * @author Binkap S <real.desert.tiger@gmail.com>
+ */
+class CXOiT_2021_11_17_CXOiT8lZ2OI3Wb9
+{
+
+    /**
+     * Table Name
+     *
+     * @var string
+     */
+    private string $table = "users";
+
+    /**
+     * Run Migration
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $column = [
+                $table->int('id')->primary(),
+                $table->varchar('userid')->unique(),
+                $table->varchar('useremail')->unique(),
+                $table->varchar('firstname')->notnull(),
+                $table->varchar('lastname')->notnull(),
+                $table->tinyint('userstatus')->default(0),
+                $table->varchar('password', 200)->notnull(),
+                $table->timestamp('created_at')->currentTime(),
+                $table->timestamp('updated_at')->currentTime()
+            ];
+            return $column;
+        });
+    }
+
+    /**
+     * Reverse Migration
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::drop($this->table);
+    }
+}
--- database/migrations/U1YcU_2021_11_17_U1YcULqttrkVE22.php
+++ database/migrations/U1YcU_2021_11_17_U1YcULqttrkVE22.php
@@ -4,11 +4,11 @@ use Imberel\Imberel\Core\Database\Schema\Blueprint;
 use Imberel\Imberel\Core\Database\Schema\Schema;
 
 /**
- *  LhgdQ_2021_11_17_LhgdQHR1UON9iQ7 Class
+ *  U1YcU_2021_11_17_U1YcULqttrkVE22 Class
  *
  * @author Binkap S <real.desert.tiger@gmail.com>
  */
-class LhgdQ_2021_11_17_LhgdQHR1UON9iQ7
+class U1YcU_2021_11_17_U1YcULqttrkVE22
 {
 
     /**
@@ -16,7 +16,7 @@ class LhgdQ_2021_11_17_LhgdQHR1UON9iQ7
      *
      * @var string
      */
-    private string $table = "random_tab";
+    private string $table = "password_reset";
 
     /**
      * Run Migration
@@ -26,10 +26,15 @@ class LhgdQ_2021_11_17_LhgdQHR1UON9iQ7
     public function up(): void
     {
         Schema::create($this->table, function (Blueprint $table) {
-            $row = [
-                $table->id(),
+            $column = [
+                $table->int('id')->primary(),
+                $table->varchar('userid')->unique(),
+                $table->varchar('useremail')->unique(),
+                $table->varchar('resetlink')->unique(),
+                $table->timestamp('created_at')->currentTime(),
+                $table->timestamp('updated_at')->currentTime()
             ];
-            return $row;
+            return $column;
         });
     }
 
