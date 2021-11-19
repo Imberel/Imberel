6228e272b6384ad7c86a3b5a67bf0b7f01db07f0
Binkap S <real.desert.tiger@gmail.com>
2021-11-19 03:57:41 +0100

    tests

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,103 +1,103 @@
 <?php
 
-function env(mixed $heystack)
-{
-    $position = \strpos($heystack, ',');
-    $key = \substr($heystack, 0, $position);
-    $value = \substr($heystack, $position + 1, \strlen($heystack));
-    if (empty(collect($key))) {
-        \putenv(\trim($key) . "=" . \trim($value));
-    }
-}
+// function env(mixed $heystack)
+// {
+//     $position = \strpos($heystack, ',');
+//     $key = \substr($heystack, 0, $position);
+//     $value = \substr($heystack, $position + 1, \strlen($heystack));
+//     if (empty(collect($key))) {
+//         \putenv(\trim($key) . "=" . \trim($value));
+//     }
+// }
 
-function cons(mixed $heystack)
-{
-    $position = \strpos($heystack, ',');
-    $key = \substr($heystack, 0, $position);
-    $value = \substr($heystack, $position + 1, \strlen($heystack));
-    \define(\trim($key), \trim($value));
-}
+// function cons(mixed $heystack)
+// {
+//     $position = \strpos($heystack, ',');
+//     $key = \substr($heystack, 0, $position);
+//     $value = \substr($heystack, $position + 1, \strlen($heystack));
+//     \define(\trim($key), \trim($value));
+// }
 
-function collect(string $key)
-{
-    return \getenv($key);
-}
+// function collect(string $key)
+// {
+//     return \getenv($key);
+// }
 
-function isession_start()
-{
-    if (collect('SESSION_DRIVER') === 'database') {
-        $session = new Imberel\Imberel\Core\Session\DatabaseSession;
-    }
-    if (collect('SESSION_DRIVER') === 'files') {
-        $session = new Imberel\Imberel\Core\Session\FilesSystemSession;
-    }
-    $session->write(USER_SESSION_ID);
-    $session->gc(collect('SESSION_LIFETIME'));
-    return $session;
-}
+// function isession_start()
+// {
+//     if (collect('SESSION_DRIVER') === 'database') {
+//         $session = new Imberel\Imberel\Core\Session\DatabaseSession;
+//     }
+//     if (collect('SESSION_DRIVER') === 'files') {
+//         $session = new Imberel\Imberel\Core\Session\FilesSystemSession;
+//     }
+//     $session->write(USER_SESSION_ID);
+//     $session->gc(collect('SESSION_LIFETIME'));
+//     return $session;
+// }
 
-function core()
-{
-    return Imberel\Imberel\Core\Application\Core::$core;
-}
+// function core()
+// {
+//     return Imberel\Imberel\Core\Application\Core::$core;
+// }
 
-function config()
-{
-    $dir = ROOTDIR . '/config/';
-    $configs = \scandir($dir, \SCANDIR_SORT_DESCENDING);
-    foreach ($configs as $config) {
-        if ($config === '.' || $config === '..') {
-            continue;
-        }
-        require_once $dir . $config;
-    }
-}
+// function config()
+// {
+//     $dir = ROOTDIR . '/config/';
+//     $configs = \scandir($dir, \SCANDIR_SORT_DESCENDING);
+//     foreach ($configs as $config) {
+//         if ($config === '.' || $config === '..') {
+//             continue;
+//         }
+//         require_once $dir . $config;
+//     }
+// }
 
-function css()
-{
-    $dir = ROOTDIR . '/resources/css/';
-    $csss = \scandir($dir, \SCANDIR_SORT_ASCENDING);
-    foreach ($csss as $css) {
-        if ($css === '.' || $css === '..') {
-            continue;
-        }
-        $css .= require_once $dir . $css;
-    }
+// function css()
+// {
+//     $dir = ROOTDIR . '/resources/css/';
+//     $csss = \scandir($dir, \SCANDIR_SORT_ASCENDING);
+//     foreach ($csss as $css) {
+//         if ($css === '.' || $css === '..') {
+//             continue;
+//         }
+//         $css .= require_once $dir . $css;
+//     }
 
-    return $css;
-}
+//     return $css;
+// }
 
-function javascript()
-{
-    $dir = ROOTDIR . '/resources/javascript/';
-    $javascripts = \scandir($dir, \SCANDIR_SORT_ASCENDING);
-    foreach ($javascripts as $javascript) {
-        if ($javascript === '.' || $javascript === '..') {
-            continue;
-        }
-        $javascript .= require_once $dir . $javascript;
-    }
+// function javascript()
+// {
+//     $dir = ROOTDIR . '/resources/javascript/';
+//     $javascripts = \scandir($dir, \SCANDIR_SORT_ASCENDING);
+//     foreach ($javascripts as $javascript) {
+//         if ($javascript === '.' || $javascript === '..') {
+//             continue;
+//         }
+//         $javascript .= require_once $dir . $javascript;
+//     }
 
-    return $javascript;
-}
+//     return $javascript;
+// }
 
-function route(Imberel\Imberel\Core\Application\Core $app)
-{
-    $router = $app->router;
-    $dir = ROOTDIR . '/routes/';
-    $routes = \scandir($dir, \SORT_DESC);
-    foreach ($routes as $route) {
-        if ($route === '.' || $route === '..') {
-            continue;
-        }
-        require_once $dir . $route;
-    }
-}
+// function route(Imberel\Imberel\Core\Application\Core $app)
+// {
+//     $router = $app->router;
+//     $dir = ROOTDIR . '/routes/';
+//     $routes = \scandir($dir, \SORT_DESC);
+//     foreach ($routes as $route) {
+//         if ($route === '.' || $route === '..') {
+//             continue;
+//         }
+//         require_once $dir . $route;
+//     }
+// }
 
-function bootstrap()
-{
-    config();
-}
+// function bootstrap()
+// {
+//     config();
+// }
 
 
 
