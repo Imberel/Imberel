cf4dac6d8cb296bbc7680f4c311329663dbccf80
Binkap S <real.desert.tiger@gmail.com>
2021-11-25 22:32:17 +0100

    added artifex.json

---
+++ app/Middleware/Sample.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Imberel\Imberel\Middleware;
+
+use Imberel\Inseminate\Interface\MiddlewareInterface;
+
+/**
+ *  Class
+ *
+ * @author Binkap S <real.desert.tiger@gmail.com>
+ */
+class Sample implements MiddlewareInterface
+{
+    // Sample Middleawre
+}
